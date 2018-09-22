1824f7d6c9ce9aba52a343302d3cb823ec766666
Cocomows <corentin.caradec@gmail.com>
2018-09-22 17:08:42 +0200

    code cleaning

--- flip/text_functions.py
+++ flip/text_functions.py
@@ -1,8 +1,18 @@
 def flip(string_to_flip):
+    """
+    Flips the given string
+    :param string_to_flip:
+    :return: string flipped
+    """
     return string_to_flip[::-1]
 
 
 def get_half_str(string_to_half):
+    """
+    Cuts the string in the middle and keeps the middle char if odd number
+    :param string_to_half:
+    :return: string halved
+    """
     if len(string_to_half) % 2 == 0:
         return string_to_half[:len(string_to_half) // 2]
     else:
@@ -10,6 +20,11 @@ def get_half_str(string_to_half):
 
 
 def sym_left(string_to_sym_left):
+    """
+    Returns symmetric string from left side
+    :param string_to_sym_left:
+    :return: symmetric string
+    """
     half = get_half_str(string_to_sym_left)
     if len(string_to_sym_left) % 2 == 0:
         return half+flip(half)
@@ -18,4 +33,9 @@ def sym_left(string_to_sym_left):
 
 
 def sym_right(string_to_sym_right):
+    """
+    Returns symmetric string from left side
+    :param string_to_sym_right:
+    :return: symmetric string
+    """
     return sym_left(flip(string_to_sym_right))
